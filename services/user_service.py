b16afe2e8e0dd68396587aadd428a489cb7a28d8
Soham Panchal <soham.panchal@qodeinvest.com>
2026-02-03 17:42:45 +0530

    update:some changes

--- services/user_service.py
+++ services/user_service.py
@@ -28,33 +28,17 @@ class UserService:
         user = None
         user_query = self.db.query(User)
         filters = []
-        identifier_fields = ["id", "uuid", "email", "username", "phone_code", "phonenumber", "pancard"]
+        identifier_fields = ["id", "email", "username", "phonenumber", "pancard"]
         for field in identifier_fields:
             value = data.get(field)
             if value is not None:
                 filters.append(getattr(User, field) == value)
+        print(filters, "==========filters")
 
         if filters:
             # Try to find a user with ANY identifier (OR), not AND
             from sqlalchemy import or_
             user = user_query.filter(or_(*filters)).first()
-
-        # If not found, attempt to find by less strict fields (name etc)
-        if user is None and (data.get("full_name") or data.get("phonenumber") or data.get("pancard")):
-            user_application_query = self.db.query(UserApplication).join(User).filter(
-                UserApplication.application_id == application_id
-            )
-            if data.get("phonenumber"):
-                user_application_query = user_application_query.filter(User.phonenumber == data["phonenumber"])
-            elif data.get("pancard"):
-                user_application_query = user_application_query.filter(User.pancard == data["pancard"])
-            else:
-                return None, None, "User not found for given profile fields"
-            user_application = user_application_query.first()
-            if user_application:
-                user = self.db.query(User).filter(User.id == user_application.user_id).first()
-            else:
-                return None, None, "User not found for given profile fields"
 
         if user is None:
             return None, None, "User not found"
